9421f30419f3d8eee1897e0a8ad7cb7bf8b705e7
Koushik Raj <koushikraj768@gmail.com>
2025-08-15 11:48:36 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,9 @@
+'''a='this is the final anniversary of the tiger'
+print(len(a))
+print(a.find('tiger'))'''
+
+a='lenovo is a good company'
+print(len(a))
+print(a.find('good'))
+if len(a)==24 and a[12]=='g':
+    print(a[:23]+'b'+a[23:])
